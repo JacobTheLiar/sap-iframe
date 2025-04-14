f3e49627fd291b634face0034fc194478effb1e0
jakub <jacob_the_liar@tlen.pl>
2025-04-14 16:42:25 +0200

    v14

--- script.js
+++ script.js
@@ -374,7 +374,7 @@ function createModalContent() {
     modalContent.style.padding = '20px';
     modalContent.style.border = '1px solid #888';
     modalContent.style.width = '645px';
-    modalContent.style.height = '385px';
+    modalContent.style.height = '580px';
     modalContent.style.borderRadius = '2rem';
     return modalContent;
 }
